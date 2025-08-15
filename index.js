aaf5496917637d016576efc4940d5f7da0364213
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-08-15 18:03:15 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const Note = require('./models/note')
 
+const app = express()
 
 let notes = []
 
@@ -14,59 +14,55 @@ const requestLogger = (request, response, next) => {
 	next()
 }
 
-app.use(express.static('dist'))
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-	Note.find({}).then(notes => {
+	Note.find({}).then((notes) => {
 		response.json(notes)
 	})
 })
 
 app.get('/api/notes/:id', (request, response) => {
-	Note.findById(request.params.id).then(note => {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note)
 	})
 })
-
-
-app.delete('/api/notes/:id', (request, response) => {
-	const id = request.params.id
-	notes = notes.filter(note => note.id !== id)
-
-	response.status(204).end()
-})
-
-const generateId = () => {
-	const maxId = notes.length > 0
-		? Math.max(...notes.map(n => Number(n.id)))
-		: 0
-	return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body
 
 	if (!body.content) {
-		return response.status(400).json({
-			error: 'content missing'
-		})
+		return response.status(400).json({ error: 'content missing' })
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
-		id: generateId(),
-	}
+	})
 
-	note.save().then(savedNote => {
+	note.save().then((savedNote) => {
 		response.json(savedNote)
 	})
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+	const id = request.params.id
+	notes = notes.filter((note) => note.id !== id)
+
+	response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -10,7 +10,6 @@ const password = process.argv[2]
 const url = `mongodb+srv://lucianlavric2005:${password}@cluster0.uswyigg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
-
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
@@ -20,8 +19,18 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result => {
-	result.forEach(note => {
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
+// })
+
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+Note.find({}).then((result) => {
+	result.forEach((note) => {
 		console.log(note)
 	})
 	mongoose.connection.close()
