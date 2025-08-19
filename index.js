e3a87d63cead52b9e8eabd413e0c4e091558fcce
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-08-19 19:03:56 -0400

    uibuild

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const requestLogger = (request, response, next) => {
 	next()
 }
 
-app.use(requestLogger)
+
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
@@ -28,7 +29,7 @@ app.get('/api/notes', (request, response) => {
 	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
 	Note.findById(request.params.id)
 		.then(note => {
 
@@ -38,11 +39,7 @@ app.get('/api/notes/:id', (request, response) => {
 				response.status(404).end()
 			}
 		})
-
-		.catch(error => {
-			console.log(error)
-			response.status(500).end()
-		})
+		.catch(error => next(error))
 })
 app.post('/api/notes', (request, response) => {
 	const body = request.body
@@ -61,11 +58,31 @@ app.post('/api/notes', (request, response) => {
 	})
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = request.params.id
-	notes = notes.filter((note) => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
+})
 
-	response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+	const { content, important } = request.body
+
+	Note.findById(request.params.id)
+		.then(note => {
+			if (!note) {
+				return response.status(404).end()
+			}
+
+			note.content = content
+			note.important = important
+
+			return note.save().then((updatedNote) => {
+				response.json(updatedNote)
+			})
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -73,6 +90,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
